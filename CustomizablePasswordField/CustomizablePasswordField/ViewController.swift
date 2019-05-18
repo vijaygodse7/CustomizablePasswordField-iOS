6ad2e458c1083129099ea74f6f10635a92746ef0
vijaygodse7 <vijaygodse007@gmail.com>
2019-05-18 21:29:03 +0530

    Initial commit - Created project and updated supported iOS version, initial VC etc.

---
+++ CustomizablePasswordField/CustomizablePasswordField/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomizablePasswordField
+//
+//  Created by Vijay Godse on 5/18/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpVC()
+    }
+    
+    private func setUpVC() {
+        view.backgroundColor = .white
+    }
+
+
+}
+
